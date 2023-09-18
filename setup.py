73624dc3bdde7721de5d8dfe3ddc22e64968f7eb
omid-b <omid.bagherpur@gmail.com>
2023-09-18 15:35:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,36 +18,29 @@ ext_kwargs = {}
 if os.environ.get("DISC_COV", None) is not None:
     ext_kwargs["define_macros"].append(("CYTHON_TRACE_NOGIL", 1))
 
-osname = os.name
-if os.name == "nt":  # windows
-    std_arg = "/std:c++17"
-else:  # posix i.e. linux and mac
-    std_arg = "-std=c++17"
-
 extensions = [
     Extension(
-        "discretize._extensions.interputils_cython",
+        name="discretize._extensions.interputils_cython",
         sources=["src/discretize/_extensions/interputils_cython.pyx"],
         include_dirs=[numpy.get_include()],
         language="c",
         **ext_kwargs
     ),
     Extension(
-        "discretize._extensions.simplex_helpers",
+        name="discretize._extensions.simplex_helpers",
         sources=["src/discretize/_extensions/simplex_helpers.pyx"],
         include_dirs=[numpy.get_include()],
         language="c",
         **ext_kwargs
     ),
     Extension(
-        "discretize._extensions.tree_ext",
+        name="discretize._extensions.tree_ext",
         sources=[
             "src/discretize/_extensions/tree_ext.pyx",
             "src/discretize/_extensions/tree.cpp",
         ],
         include_dirs=[numpy.get_include()],
         language="c++",
-        extra_compile_args=[std_arg],
         **ext_kwargs
     ),
 ]
@@ -65,11 +58,23 @@ except ImportError:
     bdist_wheel = None
 
 
-# cmdclass['_build_ext']
+# cmdclass['build_ext']
 class build_ext(_build_ext):
-    def finalize_options(self):
-        _build_ext.finalize_options(self)
-        self.include_dirs.append(numpy.get_include())
+    # add compiler specific standard argument specifier
+    def build_extension(self, ext):
+        # This module requires c++17 standard
+        if ext.name == "discretize._extensions.tree_ext":
+            comp_type = self.compiler.compiler_type
+            if comp_type == "msvc":
+                std_arg = "/std:c++17"
+            elif comp_type == "bcpp":
+                raise Exception("Must use cpp compiler that support C++17 standard.")
+            else:
+                std_arg = "-std=c++17"
+            ext.extra_compile_args = [
+                std_arg,
+            ]
+        super().build_extension(ext)
 
 
 setup_kwargs = {}
